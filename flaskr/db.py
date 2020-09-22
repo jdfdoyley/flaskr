b867b0acba2dbbc20aaa1bcb72c3726965a39d7f
Jason D'Oyley <jdfd.dev@icloud.com>
2020-09-22 01:57:00 -0400

    Register functions with app

--- flaskr/db.py
+++ flaskr/db.py
@@ -27,7 +27,7 @@ def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        db.execuescript(f.read().decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
@@ -36,3 +36,8 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
