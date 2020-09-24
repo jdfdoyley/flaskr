fb424e7209816982f58f5a39d38d98de0788337a
Jason D'Oyley <jdfd.dev@icloud.com>
2020-09-24 14:44:58 -0400

    Add delete option to blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -48,7 +48,7 @@ def create():
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post.p JOIN user u ON p.author_id = u.id'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
@@ -62,7 +62,7 @@ def get_post(id, check_author=True):
     return post
 
 
-@bp.route('<int:id>/update', methods=('GET', 'POST'))
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
     post = get_post(id)
@@ -88,3 +88,13 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
